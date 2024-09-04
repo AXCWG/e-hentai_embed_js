488097609aa40fc1fa3485a447d5ce08113f9f76
axcwg <xiey0@qq.com>
2024-09-05 00:44:15 +0800

    Details, README

--- src/App.js
+++ src/App.js
@@ -36,26 +36,28 @@ function App() {
 
     <div className='rounded' style={{ backgroundColor: "#F0F0F0", }}>
       <div className="row">
-        <div className="col-md-4 d-md-none d-block" style={{ zoom: 1, textAlign: "center" }}>
+        <div className="col-md-5 d-md-none d-block" style={{ zoom: 1, textAlign: "center" }}>
           
-          <img id="thumb" src={gmetadataObject_react.thumb.replace("l.jpg", "300.jpg")} style={{height:"100%"}} className="img-fluid p-3" alt='thumbnail'  width={"400"}/>
+          <img id="thumb" src={gmetadataObject_react.thumb.replace("l.jpg", "300.jpg")} style={{height:"100%"}} className="p-3" alt='thumbnail' />
         </div>
-        <div className="col-md-4 d-md-block d-none" style={{ zoom: 1, textAlign: "center" }}>
-          <img id="thumb" src={gmetadataObject_react.thumb} style={{height:"100%"}} className="img-fluid p-3" alt='thumbnail' />
+        <div className="col-md-5 d-md-block d-none" style={{ zoom: 1, textAlign: "center" }}>
+          <img id="thumb" src={gmetadataObject_react.thumb} style={{height:"100%"}} className="p-3" alt='thumbnail' />
         </div>
 
 
-        <div className="col-md-6 p-1" >
+        <div className="col-md-7 p-3" >
           <div className='container'>
             <div className="container" style={{ textAlign: "center", width: "100%" }}>
-
-              <h3 className="display-6 py-3" style={{ fontSize: "1.5rem", textAlign: "start", }} id="title">{gmetadataObject_react.title}</h3>
+              <h3 onClick={()=>{window.top.location='https://e-hentai.org/g/'+gmetadataObject_react.gid + '/' + gmetadataObject_react.token}} className="display-6 " style={{ fontSize: "1.5rem", textAlign: "start", }} id="title">{gmetadataObject_react.title}</h3>
+              <div style={{textAlign: "start"}} onClick={()=>{window.top.location='https://e-hentai.org/uploader/'+gmetadataObject_react.uploader}}>{gmetadataObject_react.uploader}</div>
+              <div onClick={()=>{window.top.location='https://e-hentai.org/'+gmetadataObject_react.category.replaceAll(" ", "").toLowerCase()}} style={{textAlign:"start", fontSize: "1.3rem"}}>{gmetadataObject_react.category}</div>
+              <div onClick={()=>{window.top.location='https://e-hentai.org/g/'+gmetadataObject_react.gid + '/' + gmetadataObject_react.token}} style={{textAlign:"start", fontSize: "1.3rem"}}>{gmetadataObject_react.gid}</div>
 
             </div>
             <div style={{ marginBottom: "2vm" }}>
               {gmetadataObject_react.tags.map((tag) => (
 
-                <div className='rounded px-2 py-1 m-1' style={{ backgroundColor: "gray", textDecoration: "none", color: "#F0F0F0", float: "left" }}><a style={{textDecoration: "none",color: "#F0F0F0",}} href={"https://e-hentai.org/tag/" + tag}>{tag}</a></div>
+                <div onClick={()=>{window.top.location='https://e-hentai.org/tag/'+tag}} className='rounded px-2 py-1 m-1' style={{ backgroundColor: "gray", textDecoration: "none", color: "#F0F0F0", float: "left" }}><a style={{textDecoration: "none",color: "#F0F0F0",}} >{tag}</a></div>
 
               ))}
               <br></br>
@@ -63,11 +65,7 @@ function App() {
           </div>
 
         </div>
-        <div className='col-md-2 p-1'>
-            <div>
-
-            </div>
-        </div>
+        
       </div>
 
     </div>
