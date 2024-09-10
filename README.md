14adcf7e03e4445e0311e76f38c71fb2a2955998
axcwg <xiey0@qq.com>
2024-09-10 23:42:03 +0800

    fix style, indentation, readme

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Embedding your favourate hentai in web.
 Usage: 
 
 ```
-<iframe src="http://axcwg.github.io/e-hentai_embed_js?gid=[Replace with gid]&amp;token=[Replace with token]"></iframe>
+<iframe src="http://axcwg.github.io/e-hentai_embed_js?gid=[Replace with gid]&token=[Replace with token]"></iframe>
 ```
 
 Insert the thing inside your website, replace the fields with the corresponding, then style it! 
@@ -19,7 +19,7 @@ Very simple. Examples here (also was from my blog): [example](https://axcwg.gith
 想要优雅地向别人推荐你的典藏？只需将下面的iframe贴入你的HTML网页就好！
 
 ```
-<iframe src="http://axcwg.github.io/e-hentai_embed_js?gid=[替换为gid]&amp;token=[替换为token]"></iframe>
+<iframe src="http://axcwg.github.io/e-hentai_embed_js?gid=[替换为gid]&token=[替换为token]"></iframe>
 ```
 
 非常简单。示例（来自我的博客）：[example](https://axcwg.github.io/e-hentai_example/)
--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ async function actions() {
   await fetch("https://api.e-hentai.org/api.php",
     {
       method: "POST",
-      body: '{"method": "gdata","gidlist": [['+searchParams.get("gid")+',"'+searchParams.get("token") + '"]],"namespace": 1}',
+      body: '{"method": "gdata","gidlist": [[' + searchParams.get("gid") + ',"' + searchParams.get("token") + '"]],"namespace": 1}',
       headers: new Headers({
         'Content-Type': 'application/json'
       }),
@@ -25,45 +25,48 @@ async function actions() {
 function App() {
   console.log(typeof (gmetadataObject_react))
   console.log(gmetadataObject_react)
-  if(searchParams.get("gid")===null | searchParams.get("token")=== null | gmetadataObject_react.title === undefined){
+  if (searchParams.get("gid") === null | searchParams.get("token") === null | gmetadataObject_react.title === undefined) {
     return (
       <>
-        invalid request. 
+        invalid request.
       </>
     )
   }
-  if(searchParams.get("dark")==1){
+  if (searchParams.get("dark") == 1) {
     document.body.classList.toggle('dark')
 
 
 
   }
   return (
-    
+
     <div className='rounded' style={{ backgroundColor: "#F0F0F0", }}>
-      <div className="row">
-        <div className="col-md-5 d-md-none d-block" style={{ zoom: 1, textAlign: "center" }}>
-          
-          <img id="thumb" src={gmetadataObject_react.thumb.replace("l.jpg", "300.jpg")} style={{height:"100%"}} className="p-3" alt='thumbnail' />
-        </div>
-        <div className="col-md-5 d-md-block d-none" style={{ zoom: 1, textAlign: "center" }}>
-          <img id="thumb" src={gmetadataObject_react.thumb} style={{height:"100%"}} className="p-3" alt='thumbnail' />
-        </div>
+      <div className="row" style={{height: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center',}}>
+        
+          <div className="col-md-6 d-md-none d-block" style={{ zoom: 1, textAlign: "center", display: 'flex', justifyContent: 'center', alignItems: 'center', height: "100%"  }}>
+            <div></div>
+            <img id="thumb" src={gmetadataObject_react.thumb.replace("l.jpg", "300.jpg")} style={{ width: "100%" }} className="p-3" alt='thumbnail' />
+          </div>
+          <div className="col-md-6 d-md-block d-none" style={{ zoom: 1, textAlign: "center", display: 'flex', justifyContent: 'center', alignItems: 'center', height: "100%" }}>
+            <img id="thumb" src={gmetadataObject_react.thumb.replace("l.jpg", "300.jpg")} style={{ width: "100%", }} className="p-3" alt='thumbnail' />
+          </div>
+        
 
 
-        <div className="col-md-7 p-3" >
-          <div className='container'>
-            <div className="container" style={{ textAlign: "center", width: "100%" }}>
-              <h3 onClick={()=>{window.top.location='https://e-hentai.org/g/'+gmetadataObject_react.gid + '/' + gmetadataObject_react.token}} className="display-6 " style={{ fontSize: "1.5rem", textAlign: "start", }} id="title">{gmetadataObject_react.title}</h3>
-              <div style={{textAlign: "start"}} onClick={()=>{window.top.location='https://e-hentai.org/uploader/'+gmetadataObject_react.uploader}}>{gmetadataObject_react.uploader}</div>
-              <div onClick={()=>{window.top.location='https://e-hentai.org/'+gmetadataObject_react.category.replaceAll(" ", "").toLowerCase()}} style={{textAlign:"start", fontSize: "1.3rem"}}>{gmetadataObject_react.category}</div>
-              <div onClick={()=>{window.top.location='https://e-hentai.org/g/'+gmetadataObject_react.gid + '/' + gmetadataObject_react.token}} style={{textAlign:"start", fontSize: "1.3rem"}}>{gmetadataObject_react.gid}</div>
+
+        <div className="col-md-6 p-3" >
+          <div className='container-fluid'>
+            <div className="container-fluid" style={{ textAlign: "center", width: "100%", }}>
+              <h3 onClick={() => { window.top.location = 'https://e-hentai.org/g/' + gmetadataObject_react.gid + '/' + gmetadataObject_react.token }} className="display-6 " style={{ fontSize: "1.5rem", textAlign: "start", }} id="title">{gmetadataObject_react.title}</h3>
+              <div style={{ textAlign: "start" }} onClick={() => { window.top.location = 'https://e-hentai.org/uploader/' + gmetadataObject_react.uploader }}>{gmetadataObject_react.uploader}</div>
+              <div onClick={() => { window.top.location = 'https://e-hentai.org/' + gmetadataObject_react.category.replaceAll(" ", "").toLowerCase() }} style={{ textAlign: "start", fontSize: "1.3rem" }}>{gmetadataObject_react.category}</div>
+              <div onClick={() => { window.top.location = 'https://e-hentai.org/g/' + gmetadataObject_react.gid + '/' + gmetadataObject_react.token }} style={{ textAlign: "start", fontSize: "1.3rem" }}>{gmetadataObject_react.gid}</div>
 
             </div>
             <div style={{ marginBottom: "2vm" }}>
               {gmetadataObject_react.tags.map((tag) => (
 
-                <div onClick={()=>{window.top.location='https://e-hentai.org/tag/'+tag}} className='rounded px-2 py-1 m-1' style={{ backgroundColor: "gray", textDecoration: "none", color: "#F0F0F0", float: "left" }}><a style={{textDecoration: "none",color: "#F0F0F0",}} >{tag}</a></div>
+                <div onClick={() => { window.top.location = 'https://e-hentai.org/tag/' + tag }} className='rounded px-2 py-1 m-1' style={{ backgroundColor: "gray", textDecoration: "none", color: "#F0F0F0", float: "left" }}><a style={{ textDecoration: "none", color: "#F0F0F0", }} >{tag}</a></div>
 
               ))}
               <br></br>
@@ -71,7 +74,7 @@ function App() {
           </div>
 
         </div>
-        
+
       </div>
 
     </div>
